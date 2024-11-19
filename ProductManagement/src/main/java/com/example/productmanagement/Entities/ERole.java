e534ce63701754bae12e9425ff0c810edfe47abb
Nesrine <74378418+nesrine345@users.noreply.github.com>
2024-11-19 21:11:47 +0100

    Delete ProductManagement directory

--- ProductManagement/src/main/java/com/example/productmanagement/Entities/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.example.productmanagement.Entities;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- ProductManagement/src/main/java/com/example/productmanagement/Repository/CommandeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.productmanagement.Repository;
-
-import com.example.productmanagement.Entities.Commande;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommandeRepository extends JpaRepository<Commande, Long> {
-}
